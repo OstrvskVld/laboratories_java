5b8bec3515be7e3f4b5f3958bfbc1158b182be78
OstrvskVld <vldstrvskj@gmail.com>
2024-09-02 01:39:38 +0300

    Laba 1 is done

--- Laba 1/src/Main.java
+++ Laba 1/src/Main.java
@@ -73,8 +73,8 @@ public class Main {
         }
     }
     public static void fr(){
-        int max = 0;
         int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int max = array[0];
         int min = array[0];
         for(int i = 0; i < array.length; i++){
             if(min > array[i]){
