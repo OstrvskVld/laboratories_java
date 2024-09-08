05a8e6b95005ff73826b713c4cca6e32a59f7e82
OstrvskVld <vldstrvskj@gmail.com>
2024-09-09 00:46:14 +0300

    Task 6 is done!

--- Laba 3/src/Main.java
+++ Laba 3/src/Main.java
@@ -1,15 +1,23 @@
 import java.util.*;
 
 public class Main {
-    public static class Find {
-        public <T extends Comparable<T>> T findMax(T num1, T num2) {
-            if (num1.compareTo(num2) > 0) {
-                return num1;
-            } else {
-                return num2;
-            }
+
+    public static double getArea(List<? extends Shape> shapes){
+        double area = 0;
+        for (Shape shape : shapes){
+            area += shape.getArea();
         }
+        return area;
     }
+//    public static class Find {
+//        public <T extends Comparable<T>> T findMax(T num1, T num2) {
+//            if (num1.compareTo(num2) > 0) {
+//                return num1;
+//            } else {
+//                return num2;
+//            }
+//        }
+//    }
     public static void main(String[] args) {
 //        Students students = new Students();
 //        students.addStudent(new Student(1,"John","123456789"));
@@ -59,11 +67,19 @@ public class Main {
 //        System.out.println(find.findMax(3.14, 2.71));
 //        System.out.println(find.findMax('A', 'Z'));
 
-        Pair<Integer,String> pair1 = new Pair<>(10,"Ten");
-        System.out.println("Pair1: " +pair1);
-        Pair<String, List<Integer>> pair2 = new Pair<>("Hello", Arrays.asList(1,2,3));
-        System.out.println("Pair2: " +pair2);
-        System.out.println("Pair1 equals Pair2: " +pair1.equals(pair2));
+//        Pair<Integer,String> pair1 = new Pair<>(10,"Ten");
+//        System.out.println("Pair1: " +pair1);
+//        Pair<String, List<Integer>> pair2 = new Pair<>("Hello", Arrays.asList(1,2,3));
+//        System.out.println("Pair2: " +pair2);
+//        System.out.println("Pair1 equals Pair2: " +pair1.equals(pair2));
+
+
+        List<Shape> shapes = new ArrayList<>();
+        shapes.add(new Circle(5));
+        shapes.add(new Rectangle(4, 6));
+        shapes.add(new Circle(2));
+        double area = getArea(shapes);
+        System.out.println("Total area: " + area);
 
     }
 
