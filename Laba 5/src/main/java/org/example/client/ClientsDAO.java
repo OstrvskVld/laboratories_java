db673ac2ee729b90cacba0ec8aa0977f621e4ecc
OstrvskVld <vldstrvskj@gmail.com>
2024-09-29 19:10:49 +0300

    makeOrder was created!

--- Laba 5/src/main/java/org/example/client/ClientsDAO.java
+++ Laba 5/src/main/java/org/example/client/ClientsDAO.java
@@ -23,5 +23,39 @@ public class ClientsDAO {
         this.client = client;
     }
 
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
 
+    public void printOrders(){
+        for (Order order : orders) {
+            System.out.println(order);
+        }
+    }
+
+    public void makeOrder(Order order){
+        String config_properties = "D:\\laboratories_java\\Laba 5\\config.properties";
+        try {
+            Connection connection = ConnectionDB.getConnection(config_properties);
+            String query = "INSERT INTO order(distance,Drivers_driver_id, Clients_client_id) value(?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+        try {
+            preparedStatement.setDouble(1, order.getDistance());
+            preparedStatement.setInt(2, order.getDrivers().getId());
+            preparedStatement.setInt(3, order.getClients().getId());
+
+            int rowsInserted = preparedStatement.executeUpdate();
+            System.out.println("Rows are inserted" + rowsInserted);
+            preparedStatement.close();
+            connection.close();
+
+            this.addOrder(order);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
